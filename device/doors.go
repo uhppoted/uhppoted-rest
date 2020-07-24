b84ffd6a8f8bb4170fcef3fee26192e24dd849ab
Tony Seebregts <tony.seebregts@gmail.com>
2020-07-24 13:04:32 -0700

    Initial implementation for OpenDoor API (ref. https://github.com/uhppoted/uhppoted-rest/issues/2)

--- device/doors.go
+++ device/doors.go
@@ -4,10 +4,12 @@ import (
 	"context"
 	"encoding/json"
 	"fmt"
-	"github.com/uhppoted/uhppoted-api/uhppoted"
-	"github.com/uhppoted/uhppoted-rest/errors"
 	"io/ioutil"
 	"net/http"
+	"time"
+
+	"github.com/uhppoted/uhppoted-api/uhppoted"
+	"github.com/uhppoted/uhppoted-rest/errors"
 )
 
 func GetDoor(impl *uhppoted.UHPPOTED, ctx context.Context, w http.ResponseWriter, r *http.Request) (int, interface{}, error) {
@@ -58,6 +60,87 @@ func GetDoor(impl *uhppoted.UHPPOTED, ctx context.Context, w http.ResponseWriter
 		Door interface{} `json:"door"`
 	}{
 		Door: reply,
+	}, nil
+}
+
+func OpenDoor(impl *uhppoted.UHPPOTED, ctx context.Context, w http.ResponseWriter, r *http.Request) (int, interface{}, error) {
+	deviceID := ctx.Value("device-id").(uint32)
+	door := ctx.Value("door").(uint8)
+
+	blob, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		return http.StatusInternalServerError,
+			errors.NewRESTError("open-door", "Error reading request"),
+			err
+	}
+
+	body := struct {
+		CardNumber *uint32 `json:"user-id"`
+		OTP        *uint32 `json:"otp"`
+	}{}
+
+	err = json.Unmarshal(blob, &body)
+	if err != nil {
+		return http.StatusBadRequest,
+			errors.NewRESTError("open-door", "Error parsing request"),
+			err
+	}
+
+	if body.CardNumber == nil {
+		return http.StatusBadRequest,
+			errors.NewRESTError("open-door", "Missing/invalid user ID"),
+			fmt.Errorf("Missing/invalid user ID in request body (%s)", string(blob))
+	}
+
+	rq := uhppoted.GetCardRequest{
+		DeviceID:   uhppoted.DeviceID(deviceID),
+		CardNumber: *body.CardNumber,
+	}
+
+	response, err := impl.GetCard(rq)
+	if err != nil {
+		return http.StatusInternalServerError,
+			errors.NewRESTError("open-door", fmt.Sprintf("Error retrieving card %v from device %v", *body.CardNumber, deviceID)),
+			err
+	} else if response == nil {
+		return http.StatusInternalServerError,
+			errors.NewRESTError("open-door", fmt.Sprintf("Error retrieving card %v from device %v", *body.CardNumber, deviceID)),
+			fmt.Errorf("No response returned to request for card %v from device %v", *body.CardNumber, deviceID)
+	} else {
+		card := response.Card
+
+		now := time.Now()
+		if card.From == nil || card.To == nil || now.Before(time.Time(*card.From)) || now.After(time.Time(*card.To)) {
+			return http.StatusUnauthorized,
+				errors.NewRESTError("open-door", fmt.Sprintf("Card %v is not valid for %v", card.CardNumber, now)),
+				fmt.Errorf("Card %v is not valid for %v", card, deviceID)
+		}
+
+		if door < 1 || door > 4 || !card.Doors[door-1] {
+			return http.StatusUnauthorized,
+				errors.NewRESTError("open-door", fmt.Sprintf("Card %v is does not have permission for %v, door %v", card.CardNumber, deviceID, door)),
+				fmt.Errorf("Card %v is not valid for %v, door %v", card, deviceID, door)
+		}
+	}
+
+	rqq := uhppoted.OpenDoorRequest{
+		DeviceID: uhppoted.DeviceID(deviceID),
+		Door:     door,
+	}
+
+	result, err := impl.OpenDoor(rqq)
+	if err != nil {
+		return http.StatusInternalServerError,
+			errors.NewRESTError("open-door", "Error opening door"),
+			err
+	} else if response == nil {
+		return http.StatusOK, nil, nil
+	}
+
+	return http.StatusOK, &struct {
+		Opened bool `json:"opened"`
+	}{
+		Opened: result.Opened,
 	}, nil
 }
 
